15e710bf3e8a999d49161730b020d80779ca8a97
Keith Gregory <contact@kdgregory.com>
2015-11-19 09:07:28 -0500

    initial commit - personalized skeleton

---
+++ src/main/java/com/kdgregory/sandbox/Main.java
@@ -0,0 +1,14 @@
+// Copyright (c) Keith D Gregory, all rights reserved
+package com.kdgregory.sandbox;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main
+{
+    public static void main(String[] argv) throws Exception
+    {
+
+    }
+}
